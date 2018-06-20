e5faa17f0c4d04472484f70d55b0b142039599ec
Volodymyr Yahello <vjagello93@gmail.com>
2018-06-20 17:49:30 +0300

    Add web api requests

---
+++ server/api/requests.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+
+from requests import Session
+from server.api.responses import Response, HttpResponseError, HttpResponse
+
+
+class Request(ABC):
+    """The abstraction of a specific API request."""
+
+    @abstractmethod
+    def response(self, **kwargs: Dict[Any, Any]) -> Response:
+        pass
+
+
+class Get(Request):
+    """Represent an api ``get`` request."""
+
+    def __init__(self, url: str) -> None:
+        self._session: Session = Session()
+        self._url: str = url
+
+    def response(self, **kwargs: Dict[Any, Any]) -> Response:
+        return HttpResponse(self._session.get(self._url, **kwargs, verify=False))
+
+
+class Post(Request):
+    """Represent an api ``post`` request."""
+
+    def __init__(self, url: str) -> None:
+        self._session: Session = Session()
+        self._url: str = url
+
+    def response(self, **kwargs: Dict[Any, Any]) -> Response:
+        return HttpResponse(self._session.post(self._url, kwargs, verify=False))
+
+
+class SafeRequest(Request):
+    """Represent a safe request.
+    Raise an error if specific status code is not presented.
+    """
+
+    def __init__(self, req: Request, status_code: int = 200) -> None:
+        self._req: Request = req
+        self._code: int = status_code
+
+    def response(self, **kwargs: Dict[Any, Any]) -> Response:
+        response: Response = self._req.response()
+        if response.status_code() != self._code:
+            raise HttpResponseError(
+                'HTTP response error with {} status code!!!'.format(response.status_code()))
+        return response
